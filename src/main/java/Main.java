21fa534f3e2a5afdb6e8e7a8a4139d3b6c72425e
alonsoski <j.alonso01@ufromail.cl>
2023-09-27 12:15:15 -0300

    a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Pokemon p = new Pokemon("Pikachu", 3, "Electrico");
+        p.imprimir();
+        p.acariciar();
+        p.capturar();
+        p.acariciar();
+    }
+
+
 }
--- src/main/java/Pokemon.java
+++ src/main/java/Pokemon.java
@@ -1,2 +1,62 @@
-package PACKAGE_NAME;public class Pokemon {
+public class Pokemon {
+    private String nombre;
+    private int edad;
+    private String tipo;
+    private boolean salvaje;
+    public Pokemon(String nombre, int edad,String tipo){
+        this.nombre=nombre;
+        this.edad = edad;
+        this.tipo = tipo;
+        this.salvaje = true;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setSalvaje(boolean salvaje) {
+        this.salvaje = salvaje;
+    }
+    public void acariciar(){
+        if (this.salvaje){
+            System.out.println("es salvaje, no puedes acariciarlo");
+        }else {
+            System.out.println("el pokemon sonrie");
+        }
+    }public void capturar(){
+        if(!this.salvaje){
+            System.out.println("ya fue capturado");
+        }else {
+            System.out.println("lo has capturado");
+            setSalvaje(false);
+        }
+    }public void imprimir(){
+        System.out.println("nombre:"+this.nombre);
+        System.out.println("edad:"+this.edad);
+        System.out.println("tipo:"+this.tipo);
+        System.out.println("capturado:"+this.salvaje);
+    }
+
+
 }
+
